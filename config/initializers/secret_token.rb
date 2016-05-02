6a7b37b8f46ba56f4cd8a61d066ed76fe01e193d
David Rodriguez <dwdrodri@aol.com>
2016-05-02 00:15:55 -0400

    Initial commit of files to the GIT repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Depot::Application.config.secret_key_base = '33f355cb81b6cb8d41b102a8995de7824415cb73a48326b020c4554f9c7d83fddf61a65adbe0a8f219df03e5d60ddaaad91f8b7acc19624ba56cb7844fc2d2e7'
