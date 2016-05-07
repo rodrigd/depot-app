6ee4c652b7f0a46ffbf222d526c85c20c6a0e040
David Rodriguez <dwdrodri@aol.com>
2016-05-07 06:07:50 -0400

    Validation!

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,16 @@
 # another comment
 # yet another comment
 class Product < ActiveRecord::Base
+  validates :title, :description, :image_url, presence: true
+  validates :price, numericality: {greater_than_or_equal_to: 0.01}
+  validates :title, uniqueness: true
+  validates :image_url, allow_blank: true, format: {
+    with: %r{\.(gif|jpg|png)\Z}i,
+    message: 'must be a URL for GIF, JPG, or PNG image.'
+  }
+  
+  def valid
+    puts 'running the validate method'
+    return false
+  end
 end
