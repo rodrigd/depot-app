2455b8e03a2db302203690abaeddb4be657cc729
David Rodriguez <dwdrodri@aol.com>
2016-05-10 06:41:36 -0400

    Validation

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
+  fixtures :products
   # test "the truth" do
   #   assert true
   # end
@@ -25,13 +26,11 @@ class ProductTest < ActiveSupport::TestCase
     )
     product.price = -1
     assert product.invalid?
-    assert_equal ["must be greater than or equal to .01"], 
-    product.errors[:price]
+    assert_equal ["must be greater than or equal to .01"], product.errors[:price]
     
     product.price = 0
     assert product.invalid?
-    assert_equal ["must be greater than or equal to .01"], 
-    product.errors[:price]
+    assert_equal ["must be greater than or equal to .01"], product.errors[:price]
     
     product.price = 1
     assert product.valid?
@@ -50,9 +49,17 @@ class ProductTest < ActiveSupport::TestCase
       assert new_product(name).valid?, "#{name} should be valid"
     end
     bad.each do |name|
-      puts name
       assert new_product(name).invalid?, "#{name} should not be valid."
     end
+  end
+  
+  test "product is not valid without a unique title" do
+    product = Product.new(title: products(:ruby).title,
+              description: "yyy",
+              price: 1,
+              image_url: "fred.gif")
+    assert product.invalid?
+    assert_equal ["has already been taken"], product.errors[:title]
   end
   
 end
