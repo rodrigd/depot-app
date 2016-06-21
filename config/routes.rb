48713ed878daaa3c6b7546a72154a836c9036bdf
David Rodriguez <dwdrodri@aol.com>
2016-06-20 22:59:15 -0400

    Checkin p. 175 RSS feed stuff but I could not yet figure how to get an RSS reader to subscribe to the atom feed that was created.  Annoying.

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Depot::Application.routes.draw do
   resources :orders
-
   resources :line_items
-
   resources :carts
-
   resources :words
 
   get "store/index"
-  resources :products
+  resources :products do
+    get :who_bought, on: :member
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
